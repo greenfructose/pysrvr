0bf2fd6b8b3660eaf600a50a18d6aa0d6e12830e
greenfructose <info@justinturney.com>
2022-07-15 15:39:46 -0500

    created api and some test routes

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+from api import API
+
+app = API()
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
 
 
-def app(environ, start_response):
-    response_body = b"Hello, World!"
-    status = "200 OK"
-    start_response(status, headers=[])
-    return iter([response_body])
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
